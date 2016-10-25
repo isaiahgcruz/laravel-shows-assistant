75a83b032cba7266314e78328b908c2f62d8e2cb
Isaiah Cruz <isaiahgcruz@gmail.com>
2016-10-25 14:21:24 +0800

    Created migration for episodes table

---
+++ database/migrations/2016_10_25_062048_create_episodes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEpisodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('episodes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->date('airdate');
+            $table->time('airtime');
+            $table->integer('season');
+            $table->integer('number');
+            $table->integer('runtime')->nullable();
+            $table->integer('show_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('show_id')->references('id')->on('shows');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('episodes');
+    }
+}
