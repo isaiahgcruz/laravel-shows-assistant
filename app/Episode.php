f709de7695c972c5958efe004a847bfbf38b09e0
Isaiah Cruz <isaiahgcruz@gmail.com>
2016-10-25 14:27:57 +0800

    Added episodes and shows relationship

--- app/Episode.php
+++ app/Episode.php
@@ -15,4 +15,65 @@ class Episode extends Model
         'id', 'name', 'airdate', 'airtime', 'season', 'number', 'show_id', 'airstamp',
         'runtime'
     ];
+
+     /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['title', 'start', 'end'];
+
+    /**
+     * Fetch episode's show
+     * 
+     * @return belongsTo
+     */
+    public function show()
+    {
+        return $this->belongsTo('App\Show');
+    }
+
+
+    public function getTitleAttribute()
+    {
+        $season = $this->attributes['season'];
+        $season =  sprintf("%02d", $season);
+        $number = $this->attributes['number'];
+        $number =  sprintf("%02d", $number);
+        $show = $this->show->name;
+
+        return 'S'.$season.'E'.$number.' '.$show;
+    }
+
+    public function getStartAttribute()
+    {
+        $date = $this->attributes['airdate'];
+        $time = $this->attributes['airtime'];
+        $tz = $this->show->timezone;
+        $date = new \DateTime($date . ' ' . $time . ' ' . $tz);
+
+        $newTz = new \DateTimeZone(\Config::get('app.timezone'));
+        $date->setTimezone($newTz);
+
+
+        return $date->format('Y-m-d H:i');
+
+    }
+
+    public function getEndAttribute()
+    {
+        $date = $this->attributes['airdate'];
+        $time = $this->attributes['airtime'];
+        $tz = $this->show->timezone;
+        $date = new \DateTime($date . ' ' . $time . ' ' . $tz);
+
+        $newTz = new \DateTimeZone(\Config::get('app.timezone'));
+        $date->setTimezone($newTz);
+        $runtime = ($this->attributes['runtime']) ? $this->attributes['runtime'] : false;
+        if ($runtime) {
+            $date->add(new \DateInterval('PT' . $runtime . 'M'));
+        }
+
+        return $date->format('Y-m-d H:i');
+    }
 }
