dc479cc7e0eca73a811e513aeec3b68444217568
Isaiah Cruz <isaiahgcruz@gmail.com>
2016-10-25 12:53:26 +0800

    Added Eloquence to Show model

--- app/Show.php
+++ app/Show.php
@@ -3,9 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Sofa\Eloquence\Eloquence;
 
 class Show extends Model
 {
+    use Eloquence;
+
+    /**
+     * The attributes that are searchable.
+     *
+     * @var array
+     **/
+    protected $searchableColumns = [
+        'name'
+    ];
+    
     /**
      * The attributes that are mass assignable.
      *
